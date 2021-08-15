0c7b8c864ac58071a42598fb9cbf35aeedab6894
courserav <lvsmlit@gmail.com>
2021-08-15 06:19:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+function superbowlWin(gamesArray){
+    let win = gamesArray.find(game => {
+        return game.result === "W"
+    })
+    if (win){
+        return win.year
+    }
+    else{
+        return win
+    }
+}
